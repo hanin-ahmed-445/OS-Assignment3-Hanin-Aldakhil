15f3676193a11f8622018fdab46146828609b34b
hanin-ahmed-445 <445052197@std.psau.edu.sa>
2026-05-07 17:39:18 +0300

    Protect executionLog with a lock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -83,7 +83,12 @@ class SharedResources {
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        lock.lock(); //66  
+        try {
+            executionLog.add(message);
+        } finally {
+            lock.unlock();
+        }
     }
 }
 
