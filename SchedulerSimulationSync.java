8c1468080524db65036b28fb4b608155ed0d36e0
hanin-ahmed-445 <445052197@std.psau.edu.sa>
2026-05-07 17:32:07 +0300

    Protect this critical section with a lock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -55,7 +55,12 @@ class SharedResources {
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+        lock.lock(); //33
+        try {
+            completedProcessCount++;
+        } finally {
+            lock.unlock();
+        }
     }
     
     // Method to add waiting time
